5b16c8174b0801ffe5cc4ff86ae9a77d64b743e9
thijsj008 <143096322+thijsj008@users.noreply.github.com>
2024-05-28 11:29:26 +0000

    6

--- script.js
+++ script.js
@@ -7,4 +7,27 @@ let richting = 0;
 let gameover = false;
 let appleSpawned = false;
 
+let headPosition = [8, snakeLength];
+
+
+//  laat begin van de slang zien waneer het niet klopt word het vakje zwart en anders zwart 
+const colorArray = [];
+for (let i = 0; i < 16; i++) {
+  colorArray[i] = [];
+  for (let j = 0; j < 16; j++) {
+    if (i == 8 && (snakeLength - j >= 0)) {
+      colorArray[i][j] = [1,j+1];
+    }
+    else {
+      colorArray[i][j] = [2];
+      colorArray[i][j][0] = 0;
+    }
+  }
+}
+
+let=seenApple =false;
+function update () {
+for 
+}
+
 
